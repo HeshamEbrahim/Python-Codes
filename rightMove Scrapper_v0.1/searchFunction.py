07347aeaf9d60c8836f15d2298a58533d06563a6
HeshamEbrahim <30932586+HeshamEbrahim@users.noreply.github.com>
2022-11-02 11:29:29 +0000

    Working on larger data set

--- rightMove Scrapper_v0.1/searchFunction.py
+++ rightMove Scrapper_v0.1/searchFunction.py
@@ -13,25 +13,34 @@ def search(inputText):
         if searchOne == []:
             searchOne = 0
         else:
-            searchOne = max([int(searchOne) for searchOne in searchOne])
-            if searchOne < 185:
-                searchOne = searchOne*10.7
-            else:
-                pass
+            try:
+                searchOne = max([int(searchOne) for searchOne in searchOne])
+                if searchOne < 185:
+                    searchOne = searchOne*10.7
+                else:
+                    pass
+            except:
+                searchOne = 0
 
     searchTwo = [str(searchTwo) for searchTwo in re.findall(r'\b\d+\s*ft2?\b', inputText)]
     if searchTwo == []:
         searchTwo = 0
     else:
         searchTwo = re.findall(r'\b\d+\b', str(searchTwo))
-        searchTwo = max([int(searchTwo) for searchTwo in searchTwo])
+        try:
+            searchTwo = max([int(searchTwo) for searchTwo in searchTwo])
+        except:
+            searchTwo = 0
 
     searchThree = [str(searchThree) for searchThree in re.findall(r'\b\d+\s*sq. ft.?\b', inputText)]
     if searchThree == []:
         searchThree = 0
     else:
         searchThree = re.findall(r'\b\d+\b', str(searchThree))
-        searchThree = max([int(searchThree) for searchThree in searchThree])
+        try:
+            searchThree = max([int(searchThree) for searchThree in searchThree])
+        except:
+            searchThree = 0
 
     searchFour = [str(searchFour) for searchFour in re.findall(r'\b\d+\s*saft?\b', inputText)]
     if searchFour == []:
@@ -43,15 +52,21 @@ def search(inputText):
         if searchFour == []:
             searchFour = 0
         else:
-            searchFour = max([int(searchFour) for searchFour in searchFour])
+            try:
+                searchFour = max([int(searchFour) for searchFour in searchFour])
+            except: 
+                searchFour = 0
 
     searchFive = [str(searchFive) for searchFive in re.findall(r'\b\d+\s*sqft?\b', inputText)]
     if searchFive == []:
         searchFive = 0
     else:
         searchFive = re.findall(r'\b\d+\b', str(searchFive))
-        searchFive = max([int(searchFive) for searchFive in searchFive])
-    
+        try:
+            searchFive = max([int(searchFive) for searchFive in searchFive])
+        except:
+            searchFive = 0
+
     try:
         searchOutput = max(searchOne, searchTwo, searchThree, searchFour, searchFive)
     except:
